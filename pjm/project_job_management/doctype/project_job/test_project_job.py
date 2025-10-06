fb1447ebf4776033c05490ebaee1705e9d0499f2
AravindR97 <aravindrajeshchry@gmail.com>
2025-10-06 15:48:26 +0530

    feat(Project Job): Add new doctype and define fields

---
+++ pjm/project_job_management/doctype/project_job/test_project_job.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Enfono Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectJob(FrappeTestCase):
+	pass
