ee9c57c916742a702f9cd832db610f1971335657
AravindR97 <aravindrajeshchry@gmail.com>
2025-10-08 09:50:34 +0530

    feat(Project Job Item): Populate child table with timesheet data

--- pjm/project_job_management/doctype/project_job/project_job.js
+++ pjm/project_job_management/doctype/project_job/project_job.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, Enfono Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Project Job", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Project Job", {
+    refresh(frm) {
+        frappe.db.get_list("Timesheet", {
+            filters: {
+                "parent_project": frm.doc.project,
+                "docstatus": 1
+            },
+            fields: ['name', 'start_date', 'total_hours', 'owner']
+        }).then(r => {
+            if(r && r.length > 0) {
+                frm.clear_table('job_timesheets');
+                
+                r.forEach(timesheet => {
+                    let row = frm.add_child('job_timesheets');
+                    row.timesheet = timesheet.name;
+                    row.date = timesheet.start_date;
+                    row.working_hours = timesheet.total_hours;
+                    row.created_by = timesheet.owner;
+                });
+                
+                frm.refresh_field('job_timesheets');
+                frm.doc.__unsaved = 0;
+                frm.page.clear_indicator();
+            }
+        });
+    }
+});
